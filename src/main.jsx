2d22724dd0686b846edefbbb48d3ab90f701497a
MiguelMalungo <98886465+MiguelMalungo@users.noreply.github.com>
2025-08-05 10:32:52 +0100

    Fix GitHub Pages routing: switch from BrowserRouter to HashRouter for direct URL access

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { AuthProvider } from './contexts/AuthContext.jsx'
 import { LanguageProvider } from './contexts/LanguageContext.jsx'
 import App from './App.jsx'
@@ -8,12 +8,12 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <LanguageProvider>
           <App />
         </LanguageProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 ) 
